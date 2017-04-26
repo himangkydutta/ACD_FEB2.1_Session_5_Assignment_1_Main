1d9e5f350e040c0cf8f8ff1bb07fe13f38f66bb2
himangkydutta <himangkydutta08@gmail.com>
2017-04-27 01:13:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+//Declaring and defining variables
+var name="Ankita";
+var age=28;
+var d = new Date("2015-03-25"); 
+var placOfBirth="Bangalore";
+
+//Displaying the above variable in console
+console.log('Name is :- '+name);
+console.log('Age is :- '+age);
+console.log('Date of Birth  is :- '+d);
+console.log('Place of birth is :- '+placOfBirth);
